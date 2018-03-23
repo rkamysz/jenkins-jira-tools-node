d6a7649050010883a6b3aee0ae9327790f39e3a0
rkamysz <rkamysz@goodgamestudios.com>
2018-03-24 00:33:58 +0100

    update jjt.js, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,10 @@ var nodeExternals = require('webpack-node-externals');
 module.exports = {
     mode:'production',
     target: 'node',
-    entry: "./src/appWrapper",
+    entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "bin"),
+        libraryTarget: "commonjs2",
         filename: "jjt.js"
     },
     externals: [nodeExternals()]
