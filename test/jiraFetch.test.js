e992dbc572e04cd851306a60b9208dca0af98f09
rkamysz <rkamysz@goodgamestudios.com>
2018-05-14 23:36:16 +0200

    more tests

--- test/jiraFetch.test.js
+++ test/jiraFetch.test.js
@@ -1,39 +1,42 @@
-let assert = require('assert');
-var chai = require('chai');
-let { shouldThrowError, buildHeaders, onResult } = require('./../src/jiraFetch.js');
-let expect = chai.expect;
-const throwError = function() { throw new TypeError('Illegal salmon!'); };
+const chai = require('chai');
+const chaiFetchMock = require('chai-fetch-mock');
+const fetchMock = require('fetch-mock');
+const { shouldThrowError, buildHeaders, onResult, JiraFetch } = require('./../src/jiraFetch.js');
+const expect = chai.expect;
+
 describe('JiraFetch', function () {
   describe('#onResult()', function () {
-    it.only('should throw an Error if given response is an error', function () {
-      let result = onResult(new Error());
-      //assert.throws(throwError, Error, "Error thrown");
-      console.log("....",result);
-      expect(throwError).to.throw(TypeError,"Illegal salmon!");
+    it('should throw an Error if given response is an error', function () {
+      expect(() =>{ return onResult(new Error()) }).to.throw(Error);
     });
   });
   describe('#buildHeaders()', function () {
     it('headers should be equal', function () {
       let result = buildHeaders("admin","admin");
-      assert.equal(JSON.stringify(result), '{"Content-Type":"application/json","Authorization":"Basic YWRtaW46YWRtaW4="}');
+      expect(JSON.stringify(result)).to.be.equal('{"Content-Type":"application/json","Authorization":"Basic YWRtaW46YWRtaW4="}');
     });
   });
   describe('#shouldThrowError()', function () {
     it('should return true when response is an error', function () {
-      let result = shouldThrowError(new Error())
-      assert.equal(result, true);
+      expect(shouldThrowError(new Error())).to.be.true;
     });
     it('should return true when response.status is not equal to expected status', function () {
-      let result = shouldThrowError({ status: 404 }, 200);
-      assert.equal(result, true);
+      expect(shouldThrowError({ status: 404 }, 200)).to.be.true;
     });
     it('should return false when status is equal to expected status', function () {
-      let result = shouldThrowError({ status: 200 }, 200);
-      assert.equal(result, false);
+      expect(shouldThrowError({ status: 200 }, 200)).to.be.false;
     });
     it('should return false by default', function () {
-      let result = shouldThrowError({});
-      assert.equal(result, false);
+      expect(shouldThrowError({})).to.be.false;
+    });
+  });
+  describe('#post()', function() {
+    before(() => fetchMock.post('https://www.google.com/cats', { cats: 5 }))
+    it('to have been called', function() {
+      fetch.mockResponse("");
+      let jf = new JiraFetch("admin","admin","https://www.google.com");
+      jf.post("/cats","");
+      expect(jf).to.equal("");
     });
   });
 });
